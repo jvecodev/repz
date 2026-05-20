d6ece5275a3756931de0a16aed7410eb204770e2
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-05-19 21:34:23 -0300

    Internacionalização PT-BR e EN-US #37

--- frontend/repz/src/app/app.config.ts
+++ frontend/repz/src/app/app.config.ts
@@ -1,24 +1,73 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
 
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import {
+  provideClientHydration,
+  withEventReplay
+} from '@angular/platform-browser';
+
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import {
+  provideHttpClient,
+  HttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
 import { authInterceptor } from './core/interceptors/auth-interceptor';
+
+import {
+  TranslateLoader,
+  TranslateModule
+} from '@ngx-translate/core';
+
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function httpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(
+    http,
+    './assets/i18n/',
+    '.json'
+  );
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+
     provideRouter(routes),
+
     provideClientHydration(withEventReplay()),
+
     providePrimeNG({
       theme: {
         preset: Lara,
-        options: { darkModeSelector: '[data-theme="dark"]' },
+        options: {
+          darkModeSelector: '[data-theme="dark"]'
+        },
       },
     }),
-    provideHttpClient(withInterceptors([authInterceptor])),
+
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'pt-BR',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 };
